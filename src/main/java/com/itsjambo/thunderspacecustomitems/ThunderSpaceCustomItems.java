702ac86ddd66e3e095837bb9135cdfa285c09d19
Ilya-2025 <kusilla16@gmail.com>
2025-02-23 12:11:07 +0200

    Initial commit

---
+++ src/main/java/com/itsjambo/thunderspacecustomitems/ThunderSpaceCustomItems.java
@@ -0,0 +1,17 @@
+package com.itsjambo.thunderspacecustomitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThunderSpaceCustomItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
